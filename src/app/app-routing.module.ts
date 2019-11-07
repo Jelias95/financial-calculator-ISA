ccfb0593fa54f1ba2051a254b283a3de30e7bf5a
James Elias <james.elias95@gmail.com>
2019-11-06 20:26:06 -0800

    Removed Results Page References

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { InstructionInputPageComponent } from './instruction-input-page/instruction-input-page.component';
-import { InstructionResultPageComponent } from './instruction-result-page/instruction-result-page.component';
 import { InstructionSetListingPageComponent } from './instruction-set-listing-page/instruction-set-listing-page.component';
 
 
 const routes: Routes = [
   { path: 'instruction-input', component: InstructionInputPageComponent },
-  { path: 'instruction-results', component: InstructionResultPageComponent },
   { path: 'instruction-set', component: InstructionSetListingPageComponent },
   { path: '', redirectTo: '/instruction-input', pathMatch: 'full' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { InstructionInputPageComponent } from './instruction-input-page/instruction-input-page.component';
-import { InstructionResultPageComponent } from './instruction-result-page/instruction-result-page.component';
 import { InstructionSetListingPageComponent } from './instruction-set-listing-page/instruction-set-listing-page.component';
 
 import { MatSliderModule } from '@angular/material';
@@ -21,7 +20,6 @@ import { MatListModule } from '@angular/material';
   declarations: [
     AppComponent,
     InstructionInputPageComponent,
-    InstructionResultPageComponent,
     InstructionSetListingPageComponent
   ],
   imports: [
